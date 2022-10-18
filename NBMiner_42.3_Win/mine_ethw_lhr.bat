0cd6208bd12757b4b6d3751a4f6270be6595957f
raphazik4 <rapha.marchetti@gmail.com>
2022-10-18 09:47:49 -0300

    Added ETHW option

---
+++ NBMiner_42.3_Win/mine_ethw_lhr.bat
@@ -0,0 +1,3 @@
+@cd /d "%~dp0"
+nbminer -a ethash -o stratum+tcp://ethw.poolbinance.com:1800 -o1 stratum+tcp://ethw.poolbinance.com:25 -o2 stratum+tcp://ethw.poolbinance.com:443 -u OverDynamoEthashMiningAccount.001.default --tl 80 --fee 1 --lm -log
+pause
