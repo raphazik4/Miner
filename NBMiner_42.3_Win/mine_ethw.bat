36ed9c6b72130282aaef2a5d8ccee846e6faf4e7
raphazik4 <rapha.marchetti@gmail.com>
2022-10-20 11:38:00 -0300

    Removed useless miner config

--- NBMiner_42.3_Win/mine_ethw.bat
+++
@@ -1,3 +0,0 @@
-@cd /d "%~dp0"
-nbminer -a ethash -o stratum+tcp://ethw.poolbinance.com:1800 -o1 stratum+tcp://ethw.poolbinance.com:25 -o2 stratum+tcp://ethw.poolbinance.com:443 -u OverDynamoEthashMiningAccount.001.default --tl 75 --fee 1 --fan 100 -log
-pause
